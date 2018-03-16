1e3583e37f0227438e66f82f95722c18e5da2ce7
egates <egates@erierecinc.com>
2018-03-16 15:22:24 -0400

    WIP

--- service.py
+++ service.py
@@ -2,8 +2,9 @@ import os
 import requests
 import xbmc, xbmcplugin, xbmcgui, xbmcaddon
 
-ADDON = xbmcaddon.Addon('plugin.video.psvue')
-ADDON_PATH_PROFILE = xbmc.translatePath(ADDON.getAddonInfo('profile'))
+ADDON = xbmcaddon.Addon()
+ADDON_PATH_PROFILE = xbmc.translatePath(PS_VUE_ADDON.getAddonInfo('profile'))
+PS_VUE_ADDON = xbmcaddon.Addon('plugin.video.psvue')
 # -----------------------------------------------------------------------------------------
 # EPG Code
 # Setup
@@ -13,6 +14,8 @@ ADDON_PATH_PROFILE = xbmc.translatePath(ADDON.getAddonInfo('profile'))
 #  - Set M3U play Local Path (Home Folder/userdata/addon_data/plugin.video.psvue/playlist.m3u)
 #  - Set IPTV Channel Logos - Channels Logos from XMLTV prefer M3U
 # ------------------------------------------------------------------------------------------
+
+
 def build_playlist():
     json_source = get_json(EPG_URL + '/browse/items/channels/filter/all/sort/channeltype/offset/0/size/500')
 
@@ -20,16 +23,14 @@ def build_playlist():
     m3u_file.write("#EXTM3U")
     m3u_file.write("\n")
 
-    xmltv_file = open(os.path.join(ADDON_PATH_PROFILE, "epg.xml"),"w")
-    xmltv_file.write('<?xml version="1.0" encoding="utf-8" ?>\n')
-    xmltv_file.write("<tv>\n")
-    channel_list = []
+    channel_ids = []
+    channel_names_str = ''
     for channel in json_source['body']['items']:
         title = channel['title']
         if channel['channel_type'] == 'linear':
             title = title.encode('utf-8')
             channel_id = str(channel['id'])
-            channel_list.append(channel_id)
+            channel_ids.append(channel_id)
             logo = None
             for image in channel['urls']:
                 if 'width' in image:
@@ -40,7 +41,7 @@ def build_playlist():
                         break
 
             url = 'http://localhost:8080/jsonrpc?request='
-            url += urllib.quote('{"jsonrpc":"2.0","method":"Addons.ExecuteAddon","params":{"addonid":"plugin.video.psvue","params":{"mode":"902","url":"' + CHANNEL_URL + '/' + channel_id + '"}},"id": 1}')
+            url += urllib.quote('{"jsonrpc":"2.0","method":"Addons.ExecuteAddon","params":{"addonid":"script.psvue.epg","params":{"mode":"902","url":"' + CHANNEL_URL + '/' + channel_id + '"}},"id": 1}')
 
             m3u_file.write("\n")
             channel_info = '#EXTINF:-1 tvg-id="'+channel_id+'" tvg-name="' + title + '"'
@@ -49,63 +50,48 @@ def build_playlist():
             m3u_file.write(channel_info+"\n")
             m3u_file.write(url+"\n")
 
-            xmltv_file.write('<channel id="'+channel_id+'">\n')
-            xmltv_file.write('    <display-name lang="en">'+title+'</display-name>\n')
-            xmltv_file.write('</channel>\n')
+            channel_names_str += '<channel id="' + channel_id + '">\n'
+            channel_names_str += '    <display-name lang="en">' + title + '</display-name>\n'
+            channel_names_str += '</channel>\n'
 
-    for channel_id in channel_list:
-        build_epg(channel_id, xmltv_file)
-
-    xmltv_file.write('</tv>\n')
     m3u_file.close()
-    xmltv_file.close()
+
+    channel_ids_str = ",".join(channel_ids)
+    ADDON.setSetting(id='channelIDs', value=channel_ids_str)
+    ADDON.setSetting(id='channelNamesXML', value=channel_names_str)
 
     dialog = xbmcgui.Dialog()
-    msg = 'EPG Done Building'
-    dialog.notification('EPG Finished', msg, xbmcgui.NOTIFICATION_INFO, 3000)
+    dialog.notification('PS Vue Playlist', 'The playlist has finished building', xbmcgui.NOTIFICATION_INFO, 3000)
 
 
-def build_epg(channel_id, xmltv_file):
-    """
-    <?xml version="1.0" encoding="utf-8" ?>
-    <tv>
-      <channel id="id1">
-          <display-name lang="en">Channel 1</display-name>
-      </channel>
-      <channel id="id2">
-          <display-name lang="en">Channel 2</display-name>
-      </channel>
-    ...
-      <programme start="20130215080000 +0100" stop="20130215081500 +0100" channel="id1">
-          <title lang="en">News</title>
-      </programme>
-      <programme start="20130215080500 +0100" stop="20130215083500 +0100" channel="id2">
-          <title lang="en">Movie</title>
-      </programme>
+def build_epg():
+    channel_ids = ADDON.getSetting('channelIDs').split(',')
+    channel_names_xml = ADDON.getSetting('channelNamesXML')
+    xmltv_file = open(os.path.join(ADDON_PATH_PROFILE, "epg.xml"), "w")
+    xmltv_file.write('<?xml version="1.0" encoding="utf-8" ?>\n')
+    xmltv_file.write("<tv>\n")
+    xmltv_file.write(channel_names_xml)
 
-       <programme start="20080715023000 -0600" stop="20080715040000 -0600" channel="I10436.labs.zap2it.com">
-            <title lang="en">Mystery!</title>
-            <sub-title lang="en">Foyle's War, Series IV: Casualties of War</sub-title>
-            <desc lang="en">The murder of a prominent scientist may have been due to a gambling debt.</desc>
-            <date>20070708</date>
-            <category lang="en">Anthology</category>
-            <category lang="en">Mystery</category>
-            <category lang="en">Series</category>
-            <episode-num system="dd_progid">EP00003026.0666</episode-num>
-            <episode-num system="onscreen">2706</episode-num>
-            <audio>
-              <stereo>stereo</stereo>
-            </audio>
-            <previously-shown start="20070708000000" />
-            <subtitles type="teletext" />
-        </programme>
-    ...
-    </tv>
-    title = "test " + channel_id
-    xmltv_file.write('<programme start="20170315160000 -0500" stop="20190315170000 -0500" channel="' + channel_id + '">\n')
-    xmltv_file.write('    <title lang="en">' + title + '</title>\n')
-    xmltv_file.write('</programme>\n')
-    """
+    progress = xbmcgui.DialogProgress()
+    progress.create('PS Vue EPG')
+    progress.update(0, 'Retrieving Programming Information...')
+
+    i = 1
+    for channel in channel_ids:
+        percent = int((float(i) / len(channel_ids)) * 100)
+        message = "Loading channel " + str(i) + ' of ' + str(len(channel_ids))
+        progress.update(percent, message)
+        build_epg_channel(xmltv_file, channel)
+        i += 1
+
+
+    xmltv_file.write('</tv>\n')
+    xmltv_file.close()
+    progress.update(100, 'Done!')
+    progress.close()
+
+
+def build_epg_channel(xmltv_file, channel_id):
     json_source = get_json(EPG_URL + '/timeline/live/' + channel_id + '/watch_history_size/0/coming_up_size/20')
     for strand in json_source['body']['strands']:
         if strand['id'] == 'now_playing' or strand['id'] == 'coming_up':
@@ -148,7 +134,7 @@ def build_epg(channel_id, xmltv_file):
 def get_json(url):
     headers = {
         'Accept': '*/*',
-        'reqPayload': ADDON.getSetting(id='EPGreqPayload'),
+        'reqPayload': PS_VUE_ADDON.getSetting(id='EPGreqPayload'),
         'User-Agent': UA_ANDROID_TV,
         'Accept-Encoding': 'gzip, deflate, br',
         'Accept-Language': 'en-US,en;q=0.5',
@@ -171,6 +157,18 @@ def get_json(url):
 
     return r.json()
 
+
+def load_cookies():
+    cookie_file = os.path.join(ADDON_PATH_PROFILE, 'cookies.lwp')
+    cj = cookielib.LWPCookieJar()
+    try:
+        cj.load(cookie_file, ignore_discard=True)
+    except:
+        pass
+
+    return cj
+
+
 def get_params():
     param = []
     paramstring = sys.argv[2]
@@ -188,14 +186,3 @@ def get_params():
                 param[splitparams[0]] = splitparams[1]
 
 
-try:
-    params=get_params()
-except:
-    pass
-
-url=None
-try: url=urllib.unquote_plus(params["url"])
-except: pass
-
-if url is not None:
-    epg_play_stream(url)
