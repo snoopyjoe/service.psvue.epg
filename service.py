6d02f40e0fefc7d245c871bb0419dca6304de0ae
eracknaphobia <eracknaphobia@hotmail.com>
2018-03-17 12:05:19 -0400

    WIP auto-enable iptv

--- service.py
+++ service.py
@@ -6,13 +6,20 @@ import xbmc, xbmcplugin, xbmcgui, xbmcaddon, xbmcvfs
 
 PS_VUE_ADDON = xbmcaddon.Addon('plugin.video.psvue')
 ADDON_PATH_PROFILE = xbmc.translatePath(PS_VUE_ADDON.getAddonInfo('profile'))
-IPTV_SIMPLE_ADDON = xbmcaddon.Addon('pvr.iptvsimple')
 UA_ANDROID_TV = 'Mozilla/5.0 (Linux; Android 6.0.1; Hub Build/MHC19J; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/61.0.3163.98 Safari/537.36'
 CHANNEL_URL = 'https://media-framework.totsuko.tv/media-framework/media/v2.1/stream/channel'
 EPG_URL = 'https://epg-service.totsuko.tv/epg_service_sony/service/v2'
 SHOW_URL = 'https://media-framework.totsuko.tv/media-framework/media/v2.1/stream/airing/'
 VERIFY = False
 DATE_FORMAT = "%Y-%m-%dT%H:%M:%S.%fZ"
+
+if not xbmc.getCondVisibility('System.HasAddon(pvr.iptvsimple)'):
+    xbmc.executeJSONRPC('{"jsonrpc":"2.0","method":"Addons.SetAddonEnabled","id":8,"params":{"addonid":"pvr.iptvsimple","enabled":true}}')
+    # Wait for iptv to start
+    if xbmc.Monitor().waitForAbort(10):
+        sys.exit()
+
+IPTV_SIMPLE_ADDON = xbmcaddon.Addon('pvr.iptvsimple')
 
 
 def build_playlist():
